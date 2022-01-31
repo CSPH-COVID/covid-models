37a5141daaa4ae5a6c76de15c4cd7e0ee9de6ce7
dhjacobson <jacobson.david.h@gmail.com>
2022-01-31 17:44:34 -0600

    finish new ODE; add new mock immunity

--- covid_model/analysis/misc/perf_test_ode_solvers.py
+++ covid_model/analysis/misc/perf_test_ode_solvers.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime as dt
 import json
-from charts import modeled, actual_hosps, mmwr_infections_growth_rate, re_estimates
+from covid_model.analysis.charts import modeled, actual_hosps, mmwr_infections_growth_rate, re_estimates
 from time import perf_counter
 from timeit import timeit
 
 from covid_model.db import db_engine
 from covid_model.model import CovidModel
-from covid_model.model_with_omicron import CovidModelWithVariants
+# from covid_model.model_with_omicron import CovidModelWithVariants
+from covid_model.model_with_immunity_rework import CovidModelWithVariants
 from covid_model.model_specs import CovidModelSpecifications
 
 if __name__ == '__main__':
@@ -20,7 +21,7 @@ if __name__ == '__main__':
 
     print('Prepping model...')
     # print(timeit('model.prep(521, engine=engine)', number=1, globals=globals()), 'seconds to prep model.')
-    model.prep(528, engine=engine, params='input/params.json', attribute_multipliers='input/attribute_multipliers.json')
+    model.prep(551, engine=engine, params='input/params.json', attribute_multipliers='input/attribute_multipliers.json')
 
     # for method in ['RK45', 'RK23', 'BDF', 'LSODA', 'Radau', 'DOP853']:
     for method in ['RK45']:
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     # cmpts_json_attrs = ('age', 'vacc')
     # print(model.solution_sum([vals_json_attr] + list(cmpts_json_attrs)).stack(cmpts_json_attrs).index.droplevel('t').to_frame().to_dict(orient='records'))
 
-    model.write_to_db(engine, cmpts_json_attrs=('age', 'vacc', 'variant'))
+    # model.write_to_db(engine, cmpts_json_attrs=('age', 'vacc', 'variant'))
 
     fig, ax = plt.subplots()
     modeled(model, 'Ih')
