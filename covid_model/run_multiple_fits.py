935a33e0a01b262fe013866e6f20d87df5541dfd
dhjacobson <jacobson.david.h@gmail.com>
2022-03-16 13:24:01 -0500

    add multiple fits and future variants

--- covid_model/run_multiple_fits.py
+++ covid_model/run_multiple_fits.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import json
@@ -8,7 +10,6 @@ from db import db_engine
 from model_fit import CovidModelFit
 from analysis.charts import actual_hosps, modeled
 from covid_model.cli_specs import ModelSpecsArgumentParser
-
 
 regions = {
   "ad": "Adams County",
@@ -36,7 +37,6 @@ regions = {
   "wcp": "West Central Partnership"
 }
 
-
 def run():
     # get fit params
     parser = ModelSpecsArgumentParser()
@@ -49,6 +49,8 @@ def run():
     parser.add_argument("-rh", "--hosp_data", type=str, help="the path to the hospitalizations data for regions (temporary fix)")
     parser.add_argument("-wb", "--write_batch_output", action="store_true", default=False, help="write the output of each batch to the database")
     parser.add_argument("-plt", "--plot", action="store_true", default=False, help="open a plot when fitting is complete, comparing modeled hosp to actual; default False")
+    parser.add_argument("-ambs", "--attribute_multipliers_by_scen", type=str, help="file containing attribute multipliers, broken out by scenario")
+    parser.add_argument("-ct", "--context_tag", type=str, help="a tag that will be added to the specification as the \"context\"")
     parser.set_defaults(refresh_vacc=False)
     fit_params = parser.parse_args()
     look_back = fit_params.look_back
@@ -62,45 +64,57 @@ def run():
     # run fit
     engine = db_engine()
 
-    fit = CovidModelFit(engine=engine, region_params=region_params, region=fit_params.region, **parser.specs_args_as_dict())
+    with open(fit_params.attribute_multipliers_by_scen) as f:
+        attr_mults_by_scen = json.load(f)
 
-    # extract county IDs for the provided region; if statewide, set counties=None
-    if region is not None:
-        counties = json.load(open(region_params))[region]['county_names']
-        counties = counties if type(counties) == list else [counties]
-        county_ids = json.load(open(region_params))[region]['county_fips']
-        county_ids = county_ids if type(county_ids) == list else [county_ids]
-    else:
-        counties, county_ids = (None, None)
+    common_attr_mults = attr_mults_by_scen['common']
+    for scen, scen_attr_mults in attr_mults_by_scen['scens'].items():
+        print(f'Running fit for scenario: "{scen}"')
+        attr_mults = common_attr_mults + scen_attr_mults
+        specs_args = {**parser.specs_args_as_dict(), 'attribute_multipliers': attr_mults}
+        fit = CovidModelFit(engine=engine, region_params=fit_params.region_params, region=fit_params.region, **specs_args)
 
-    # if hospitalization data CSV is provided, get hospitalizations from there; else query the database
-    if fit_params.hosp_data is not None:
+        # extract county IDs for the provided region; if statewide, set counties=None
         if region is not None:
-            hosp_data = pd.read_csv(fit_params.hosp_data)[['date'] + counties]
-            tstart = pd.to_datetime(hosp_data['date']).min()
-            fit.base_specs.start_date = dt.date(tstart.year, tstart.month, tstart.day)
-            fit.actual_hosp = hosp_data.drop('date', axis=1).sum(axis=1)
+            counties = json.load(open(region_params))[region]['county_names']
+            counties = counties if type(counties) == list else [counties]
+            county_ids = json.load(open(region_params))[region]['county_fips']
+            county_ids = county_ids if type(county_ids) == list else [county_ids]
         else:
-            raise ValueError('Hospitalization data file is not supported for statewide model fitting.')
-    else:
-        fit.set_actual_hosp(engine, county_ids=county_ids)
+            counties, county_ids = (None, None)
 
-    # run fit
-    fit.run(engine, look_back=look_back, batch_size=batch_size,
-            increment_size=increment_size, window_size=window_size,
-            region_params=region_params, region=region,
-            write_batch_output=write_batch_output)
+        # if hospitalization data CSV is provided, get hospitalizations from there; else query the database
+        if fit_params.hosp_data is not None:
+            if region is not None:
+                hosp_data = pd.read_csv(fit_params.hosp_data)[['date'] + counties]
+                tstart = pd.to_datetime(hosp_data['date']).min()
+                fit.base_specs.start_date = dt.date(tstart.year, tstart.month, tstart.day)
+                fit.actual_hosp = hosp_data.drop('date', axis=1).sum(axis=1)
+            else:
+                raise ValueError('Hospitalization data file is not supported for statewide model fitting.')
+        else:
+            fit.set_actual_hosp(engine, county_ids=county_ids)
 
-    print(fit.fitted_model.tslices)
-    print(fit.fitted_tc)
+        # run fit
+        fit.run(engine, look_back=look_back, batch_size=batch_size,
+                increment_size=increment_size, window_size=window_size,
+                region_params=region_params, region=region,
+                write_batch_output=write_batch_output)
 
-    fit.fitted_model.tags['run_type'] = 'fit'
-    fit.fitted_model.write_to_db(engine)
+        print(fit.fitted_model.tslices)
+        print(fit.fitted_tc)
 
-    if fit_params.plot:
-        actual_hosps(engine, county_ids=county_ids)
-        modeled(fit.fitted_model, 'Ih')
-        plt.show()
+        fit.fitted_model.tags['run_type'] = 'scenario fit'
+        fit.fitted_model.tags['context'] = fit_params.context_tag
+        fit.fitted_model.tags['scenario'] = scen
+        fit.fitted_model.write_to_db(engine)
+
+        if fit_params.plot:
+            actual_hosps(engine)
+            modeled(fit.fitted_model, 'Ih')
+            plt.draw()
+
+    plt.show()
 
 
 if __name__ == '__main__':
