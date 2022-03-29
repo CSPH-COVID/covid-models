893e20127da318850c529908241e6ce5fff2e40d
Alex Fout <fouticus@users.noreply.github.com>
2022-03-29 16:08:32 -0600

    added plots for non-normalized compartment breakouts.

--- covid_model/analysis/run_compartment_report.py
+++ covid_model/analysis/run_compartment_report.py
@@ -43,6 +43,24 @@ if __name__ == '__main__':
 
     fig.tight_layout()
     if args.save_prefix:
+        plt.savefig("output/" + args.save_prefix + "_compartment_report_grpby_" + "_".join(args.group_by_attr_names) + "_share_of_total.png")
+    else:
+        plt.show()
+    plt.close()
+
+    # repeat but don't normalize to a percentage.
+    fig, axs = plt.subplots(len(seir), len(args.group_by_attr_names), figsize=(5 * len(seir), 5 * len(args.group_by_attr_names)), dpi=300)
+    for i, (seir_label, seir_attrs) in enumerate(seir.items()):
+        for j, attr_name in enumerate(args.group_by_attr_names):
+            ax = axs[i, j]
+            modeled(model, seir_attrs, groupby=attr_name, share_of_total=False, ax=ax)
+            ax.set_xlim(args.from_date, args.to_date)
+            #ax.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+            ax.set_ylabel(f'{seir_label} by {attr_name}')
+            ax.legend(loc='upper left')
+
+    fig.tight_layout()
+    if args.save_prefix:
         plt.savefig("output/" + args.save_prefix + "_compartment_report_grpby_" + "_".join(args.group_by_attr_names) + ".png")
     else:
         plt.show()
