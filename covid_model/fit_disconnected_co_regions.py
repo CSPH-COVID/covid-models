f1e51d34776a2a6b26482c953fed9c51ec5391ba
Alex Fout <fouticus@users.noreply.github.com>
2022-03-29 16:14:13 -0600

    parallelized the disconnected region fitting. results are collected and written serially to the database to avoid concurrency issues.

--- covid_model/fit_disconnected_co_regions.py
+++ covid_model/fit_disconnected_co_regions.py
@@ -1,13 +1,64 @@
 import matplotlib.pyplot as plt
 import json
 import datetime as dt
+from multiprocessing import Pool
+from functools import partial
 
 from db import db_engine
 from covid_model import RegionalCovidModel, all_regions
+from covid_model.model_specs import CovidModelSpecifications
 from model_fit import CovidModelFit
 from analysis.charts import actual_hosps, modeled
 from covid_model.cli_specs import ModelSpecsArgumentParser
 from data_imports import ExternalHosps
+
+
+def region_fit(args):
+    region, region_params, fit_params, specs_args, look_back, batch_size, increment_size, window_size, write_batch_output = args
+    rname = all_regions[region]
+    print(f'Region: {rname}')
+    try:
+        engine = db_engine()
+        # get hospitalization data and adjust start/end dates to fit hospitalizations
+        print(f"{rname}: Adjusting start / end dates")
+        region_counties = json.load(open(region_params))[region]['county_names']
+        region_county_ids = json.load(open(region_params))[region]['county_fips']
+        hosps = ExternalHosps(engine).fetch_from_db(region_county_ids)
+        start_date = max(dt.datetime.strptime(fit_params.start_date, "%Y-%m-%d").date(),
+                         min(hosps.index[hosps['currently_hospitalized'] > 0]) - dt.timedelta(days=1)).strftime(
+            "%Y-%m-%d")
+        end_date = min(dt.datetime.strptime(fit_params.end_date, "%Y-%m-%d").date(),
+                       max(hosps.index[hosps['currently_hospitalized'] > 0])).strftime("%Y-%m-%d")
+        specs_args.update({'start_date': start_date, 'end_date': end_date})
+        print(f'{rname}: Adjusted start and end dates: {specs_args["start_date"]}, {specs_args["end_date"]}')
+        print(f'{rname}: Creating fit object and loading hospitalizations')
+        fit = CovidModelFit(engine=engine, region_params=fit_params.region_params, region=region,
+                            tc_min=fit_params.tc_min, tc_max=fit_params.tc_max, **specs_args)
+        fit.set_actual_hosp(engine, county_ids=region_county_ids)
+
+        # run fit
+        print(f'{rname}: Fitting')
+        fit.run(engine, look_back=look_back, batch_size=batch_size,
+                increment_size=increment_size, window_size=window_size,
+                region_params=region_params, region=region,
+                write_batch_output=write_batch_output,
+                model_class=RegionalCovidModel,
+                model_args={"region": region},
+                forward_sim_each_batch=True, use_base_specs_end_date=True)
+
+        fit.fitted_model.tags['run_type'] = 'fit'
+
+        if fit_params.plot:
+            print(f'{rname}: Plotting')
+            actual_hosps(engine, county_ids=region_county_ids)
+            modeled(fit.fitted_model, 'Ih')
+            plt.savefig('output/fitted_' + region + ".png", dpi=300)
+        print(f'{rname}: Succeeded, spec_id:{fit.fitted_model.spec_id}')
+        return fit.fitted_model.preform_write_query()
+    except RuntimeError as e:
+        print(e)
+        print(f'{rname}: Failed')
+        return None
 
 
 def run():
@@ -23,6 +74,7 @@ def run():
     parser.add_argument("-rg", "--regions", nargs="+", choices=all_regions.keys(), required=False, help="Specify the regions to be run, default is all regions (not counties)")
     parser.add_argument("-wb", "--write_batch_output", action="store_true", default=False, help="write the output of each batch to the database")
     parser.add_argument("-plt", "--plot", action="store_true", default=False, help="open a plot when fitting is complete, comparing modeled hosp to actual; default False")
+    parser.add_argument("-mp", "--multiprocess", default=None, help="if present, indicates how many indipendent processes to spawn in a threadpool for fitting the models")
     fit_params = parser.parse_args()
     look_back = fit_params.look_back
     batch_size = fit_params.batch_size if fit_params.batch_size is not None else look_back
@@ -31,47 +83,20 @@ def run():
     regions = fit_params.regions
     region_params = fit_params.region_params
     write_batch_output = fit_params.write_batch_output
+    multiprocess = int(fit_params.multiprocess) if fit_params.multiprocess else None
+    specs_args = parser.specs_args_as_dict()
 
-    engine = db_engine()
+    args = [region_params, fit_params, specs_args, look_back, batch_size, increment_size, window_size, write_batch_output]
 
-    # run fit
-    for region in regions:
-        print(f'Region: {all_regions[region]}')
-        try:
-            # get hospitalization data and adjust start/end dates
-            print("Adjusting start / end dates")
-            region_counties = json.load(open(region_params))[region]['county_names']
-            region_county_ids = json.load(open(region_params))[region]['county_fips']
-            hosps = ExternalHosps(db_engine()).fetch_from_db(region_county_ids)
-            fit_params.start_date = max(dt.datetime.strptime(fit_params.start_date, "%Y-%m-%d").date(),
-                                        min(hosps.index[hosps['currently_hospitalized'] > 0]) - dt.timedelta(days=1)).strftime("%Y-%m-%d")
-            fit_params.end_date = min(dt.datetime.strptime(fit_params.end_date, "%Y-%m-%d").date(),
-                                        max(hosps.index[hosps['currently_hospitalized'] > 0])).strftime("%Y-%m-%d")
-            print(f'Adjusted start and end dates: {fit_params.start_date}, {fit_params.end_date}')
-            print('Creating fit object and loading hospitalizations')
-            fit = CovidModelFit(engine=engine, region_params=fit_params.region_params, region=region, tc_min=fit_params.tc_min, tc_max=fit_params.tc_max, **parser.specs_args_as_dict())
-            fit.set_actual_hosp(engine, county_ids=region_county_ids)
+    if multiprocess:
+        p = Pool(multiprocess)
+        write_infos = p.map(region_fit, map(lambda x: [x] + args, regions))
+    else:
+        write_infos = map(region_fit, map(lambda x: [x] + args, regions))
 
-            # run fit
-            print('Fitting')
-            fit.run(engine, look_back=look_back, batch_size=batch_size,
-                    increment_size=increment_size, window_size=window_size,
-                    region_params=region_params, region=region,
-                    write_batch_output=write_batch_output,
-                    model_class=RegionalCovidModel,
-                    model_args={"region": region} if regions is not None else dict(),
-                    forward_sim_each_batch=True)
-
-            fit.fitted_model.tags['run_type'] = 'fit'
-            fit.fitted_model.write_to_db(engine)
-            if fit_params.plot:
-                print('Plotting')
-                actual_hosps(engine, county_ids=region_county_ids)
-                modeled(fit.fitted_model, 'Ih')
-                plt.savefig('output/fitted_' + region + ".png", dpi=300)
-            print(f'{region} succeeded')
-        except RuntimeError:
-            print(f'{region} failed')
+    for write_info in write_infos:
+        if write_info is not None:
+            CovidModelSpecifications.write_preformed_to_db(write_info, db_engine())
 
 
 if __name__ == '__main__':
