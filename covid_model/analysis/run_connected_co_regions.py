b77749c18f83dd5b01d27df66bf8d6d1047c9bc4
Alex Fout <fouticus@users.noreply.github.com>
2022-04-08 10:47:17 -0600

    added comparison between mobility and no mobility options

--- covid_model/analysis/run_connected_co_regions.py
+++ covid_model/analysis/run_connected_co_regions.py
@@ -1,5 +1,6 @@
 ### Python Standard Library ###
 ### Third Party Imports ###
+import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 ### Local Imports ###
@@ -13,21 +14,26 @@ def run():
     parser = ModelSpecsArgumentParser()
     specs_args = parser.specs_args_as_dict()
 
-    # build / solve model
-    engine = db_engine()
-    model = CovidModel(engine=engine, **specs_args)
-    model.prep()
-    model.solve_seir()
+
+    # Model with no mobility
+    dfs = []
+    for mm in ['none', 'population_attached']:
+        print(f"mobility_mode: {mm}")
+        specs_args['mobility_mode'] = mm
+        engine = db_engine()
+        model = CovidModel(engine=engine, **specs_args)
+        model.prep()
+        model.solve_seir()
+        dfs.append(model.solution_sum(['seir', 'region']).stack([0, 1]).reset_index(name='y').assign(mobility=mm))
+    df = pd.concat(dfs)
 
     # plot
-    df = model.solution_sum(['seir', 'region']).stack([0, 1]).reset_index(name='y')
     #df['seir_age'] = df['seir'] + " : " + df['age']
-    g = sns.FacetGrid(df, row='seir', hue='seir', col='region', height=4, aspect=3, sharey=False)
+    g = sns.FacetGrid(df, row='seir', hue='mobility', col='region', height=4, aspect=3, sharey=False)
     g.map(sns.lineplot, 't', 'y')
     g.add_legend()
     plt.tight_layout()
     plt.savefig("covid_model/output/connected_forwardsim_test.png")
-
 
     print("end")
 
