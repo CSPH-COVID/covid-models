863ed287c8d9df325e67fdd9f003408f277932ee
dhjacobson <jacobson.david.h@gmail.com>
2022-04-12 14:27:58 -0500

    add scatterplots

--- covid_model/analysis/run_model_evaluation.py
+++ covid_model/analysis/run_model_evaluation.py
@@ -18,12 +18,22 @@ if __name__ == '__main__':
     parser.add_argument('-skip', '--skip_first_increments', type=int, default=0, help='skip this many prediction increments at the beginning of the pandemic')
     run_args = parser.parse_args()
 
-    fig, axs = plt.subplots(3)
+    fig = plt.figure(figsize=(26, 12))
+
+    gs = fig.add_gridspec(4, 2)
+    ax_hosp = fig.add_subplot(gs[0, 0])
+    ax_rmse_by_start_date = fig.add_subplot(gs[1, 0])
+    ax_rmse_by_day_of_proj = fig.add_subplot(gs[2, 0])
+    ax_hosp_scatter = fig.add_subplot(gs[:2, 1])
+    ax_trend_scatter = fig.add_subplot(gs[2:, 1])
 
     print('Prepping base model...')
     engine = db_engine()
     base_model = CovidModel(engine=engine, **parser.specs_args_as_dict())
-    actual_hospitalizations = ExternalHosps(engine, t0_date=base_model.start_date).fetch(county_ids=None)['currently_hospitalized'].rename('Actual Hospitalizations')
+    actual_hospitalizations = ExternalHosps(engine, t0_date=base_model.start_date).fetch(county_ids=None)['currently_hospitalized'].rename('Actual Hospitalizations').to_frame()
+    for period in [7, 14]:
+        actual_hospitalizations[f'Actual Hospitalizations ({period}-Day)'] = actual_hospitalizations['Actual Hospitalizations'].rolling(period).mean()
+        actual_hospitalizations[f'Trend ({period}-Day)'] = actual_hospitalizations[f'Actual Hospitalizations ({period}-Day)'] / actual_hospitalizations[f'Actual Hospitalizations ({period}-Day)'].shift(period) - 1
 
     base_model.prep()
     base_model.solve_seir()
@@ -63,28 +73,46 @@ if __name__ == '__main__':
     # build dataframe
     projected_hospitalizations = pd.concat(projections, names=['Projection Start Date', 'Projection Method', 't'])
     df = pd.merge(projected_hospitalizations, actual_hospitalizations, left_on='t', right_index=True).reset_index()
+    df['projection_start_t'] = (df['Projection Start Date'] - base_model.start_date).dt.days
     df['Date'] = base_model.start_date
     df['Date'] += pd.to_timedelta(df['t'], unit='d')
     df['Day of Projection'] = (df['Date'] - df['Projection Start Date']).dt.days
     df['Error'] = df['Projected Hospitalizations'] - df['Actual Hospitalizations']
     df['Sq. Error'] = np.square(df['Error'])
 
+    for col in actual_hospitalizations.columns:
+        df = pd.merge(df, actual_hospitalizations[col].rename(f'{col} at Projection Start Date'), left_on='projection_start_t', right_index=True)
+
+    # df = df.set_index(['Projection Start Date', 'Projection Method', 'Date']).sort_index()
+    # for period in [7, 14]:
+    #     df[f'Actual Hospitalizations ({period}-Day)'] = df.groupby(['Projection Start Date', 'Projection Method'], as_index=False).rolling(period).mean()['Actual Hospitalizations']
+    #     df[f'Trend ({period}-Day)'] = df[f'Actual Hospitalizations ({period}-Day)'] / df.groupby(['Projection Start Date', 'Projection Method'], as_index=False).shift(period)[f'Actual Hospitalizations ({period}-Day)'] - 1
+    #     import pdb; pdb.set_trace()
+    #     df = df.join(df[f'Actual Hospitalizations ({period}-Day)'].rename(f'Actual Hospitalizations ({period}-Day) at Projection Start Date').rename_axis(index=['aaaa', 'bbbb', 'Projection Start Date']), on='Projection Start Date', how='left')
+    #     df = pd.merge(df, df[f'Actual Hospitalizations ({period}-Day)'].rename(f'Actual Hospitalizations ({period}-Day) at Projection Start Date'), left_on='Projection Start Date', right_on='Date')
+    #     df = pd.merge(df, df[f'Trend ({period}-Day)'].rename(f'Trend ({period}-Day) at Projection Start Date'), left_on='Projection Start Date', right_on='Date')
+    # df = df.reset_index()
+
     # plot
     colors = ['navy', 'crimson', 'deepskyblue']
-    sns.lineplot(data=df, x='Date', y='Projected Hospitalizations', hue='Projection Method', style='Projection Start Date', dashes=['']*len(df['Projection Start Date'].unique()), ax=axs[0], legend=False, palette=colors)
-    sns.lineplot(data=np.sqrt(df.groupby(['Projection Method', 'Projection Start Date'])['Sq. Error'].mean()).to_frame(), x='Projection Start Date', y='Sq. Error', hue='Projection Method', ax=axs[1], palette=colors)
-    sns.lineplot(data=np.sqrt(df.groupby(['Projection Method', 'Day of Projection'])['Sq. Error'].mean()).to_frame(), x='Day of Projection', y='Sq. Error', hue='Projection Method', ax=axs[2], palette=colors)
+    sns.lineplot(data=df, x='Date', y='Projected Hospitalizations', hue='Projection Method', style='Projection Start Date', dashes=['']*len(df['Projection Start Date'].unique()), ax=ax_hosp, legend=False, palette=colors)
+    sns.lineplot(data=np.sqrt(df.groupby(['Projection Method', 'Projection Start Date'])['Sq. Error'].mean()).to_frame(), x='Projection Start Date', y='Sq. Error', hue='Projection Method', ax=ax_rmse_by_start_date, palette=colors)
+    sns.lineplot(data=np.sqrt(df.groupby(['Projection Method', 'Day of Projection'])['Sq. Error'].mean()).to_frame(), x='Day of Projection', y='Sq. Error', hue='Projection Method', ax=ax_rmse_by_day_of_proj, palette=colors)
+    sns.regplot(data=df.groupby(['Projection Method', 'Projection Start Date'])[['Actual Hospitalizations (14-Day) at Projection Start Date', 'Error']].mean(), x='Actual Hospitalizations (14-Day) at Projection Start Date', y='Error', ax=ax_hosp_scatter)
+    sns.regplot(data=df.groupby(['Projection Method', 'Projection Start Date'])[['Trend (14-Day) at Projection Start Date', 'Error']].mean(), x='Trend (14-Day) at Projection Start Date', y='Error', ax=ax_trend_scatter)
     # sns.heatmap(data=)
-    actual_hosps(engine, color='black', ax=axs[0])
+    actual_hosps(engine, color='black', ax=ax_hosp)
 
-    axs[0].set_ylim(0, 4000)
+    ax_hosp.set_ylim(0, 4000)
 
-    axs[0].set_ylabel('Hospitalized with COVID-19')
-    axs[1].set_ylabel('Root-Mean-Square Error')
-    axs[2].set_ylabel('Root-Mean-Square Error')
+    ax_hosp.set_ylabel('Hospitalized with COVID-19')
+    ax_rmse_by_start_date.set_ylabel('Root-Mean-Square Error')
+    ax_rmse_by_day_of_proj.set_ylabel('Root-Mean-Square Error')
 
-    for ax in axs.flatten():
+    for ax in fig.axes:
         ax.grid('lightgray')
+
+    fig.tight_layout()
     plt.show()
 
 
