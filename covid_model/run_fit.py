8081744b8c3970cc3d3daf426acd50056fffa419
Alex Fout <fouticus@users.noreply.github.com>
2022-04-13 15:12:03 -0600

    removed hosp data loading from file, allowed parallelization to speed up multiple region fits.

--- covid_model/run_fit.py
+++ covid_model/run_fit.py
@@ -1,15 +1,38 @@
 ### Python Standard Library ###
-import json
-import datetime as dt
+from multiprocessing import Pool
 ### Third Party Imports ###
 import matplotlib.pyplot as plt
 import pandas as pd
 ### Local Imports ###
+from covid_model.model_specs import CovidModelSpecifications
 from covid_model.db import db_engine
-import covid_model
 from covid_model.model_fit import CovidModelFit
 from covid_model.analysis.charts import actual_hosps, modeled
 from covid_model.cli_specs import ModelSpecsArgumentParser
+
+
+def do_fit_on_region(args):
+    region, specs_args, non_specs_args, perform_specs_write = args
+
+    # initialize fit object
+    engine = db_engine()
+    fit = CovidModelFit(engine=engine, tc_min=non_specs_args['tc_min'], tc_max=non_specs_args['tc_max'], **specs_args)
+    fit.set_actual_hosp(engine=engine, county_ids=fit.base_specs.get_all_county_fips())
+
+
+    # run fit
+    fit.run(engine, **non_specs_args, print_prefix=f'{region}:')
+    print(fit.fitted_model.tslices)
+    print(fit.fitted_tc)
+
+    if non_specs_args['plot']:
+        # TODO: update
+        # actual_hosps(engine, county_ids=)
+        modeled(fit.fitted_model, 'Ih')
+        plt.show()
+
+    fit.fitted_model.tags['run_type'] = 'fit'
+    return fit.fitted_model.write_specs_to_db(engine=engine) if perform_specs_write else fit.fitted_model.prepare_write_specs_query()
 
 
 def run():
@@ -21,66 +44,31 @@ def run():
     parser.add_argument("-ws", "--window_size", type=int, help="the number of days in each TC-window; default to 14")
     parser.add_argument("-tmn", "--tc_min", type=float, default=0, help="The lowest tc to allow")
     parser.add_argument("-tmx", "--tc_max", type=float, default=0.99, help="The lowest tc to allow")
-    parser.add_argument("-rd", "--region_definitions", type=str, default="input/region_definitions.json", help="the path to the region-specific params file to use for fitting; default to 'input/region_definitions.json'")
-    parser.add_argument("-rg", "--region", choices=covid_model.all_regions_and_counties.keys(), required=False, help="Specify the region to be run, if not specified, just runs default parameters")
-    parser.add_argument("-rh", "--hosp_data", type=str, help="the path to the hospitalizations data for regions (temporary fix)")
+    parser.add_argument("-bsed", '--use_base_specs_end_date', action="store_true", default=False, help="use end date from model specifications, not hospitalizations data")
     parser.add_argument("-wb", "--write_batch_output", action="store_true", default=False, help="write the output of each batch to the database")
-    parser.add_argument("-mc", "--model_class", default="CovidModel", help="Name of the class to use for the model (Defaults to CovidModel)")
+    parser.add_argument("-mup", "--multiprocess", type=int, default=None, help="if present, indicates how many indipendent processes to spawn in a threadpool for fitting the models")
+    parser.add_argument("-fs", "--forward_sim_each_batch", action='store_true', default=False, help="after each tc fit, plot the hospitalized vs. fitted, along with TC")
     parser.add_argument("-plt", "--plot", action="store_true", default=False, help="open a plot when fitting is complete, comparing modeled hosp to actual; default False")
-    parser.set_defaults(refresh_vacc=False)
+    parser.set_defaults(refresh_vacc=False, increment_size=1, window_size=14, batch_size=1)
+
     fit_params = parser.parse_args()
-    look_back = fit_params.look_back
-    batch_size = fit_params.batch_size if fit_params.batch_size is not None else look_back
-    increment_size = fit_params.increment_size if fit_params.increment_size is not None else 1
-    window_size = fit_params.window_size if fit_params.window_size is not None else 14
-    region_definitions = fit_params.region_definitions
-    region = fit_params.region
-    write_batch_output = fit_params.write_batch_output
-    model_class = getattr(covid_model, fit_params.model_class)
+    batch_size = fit_params.batch_size if fit_params.batch_size is not None else fit_params.look_back
 
-    # run fit
-    engine = db_engine()
-    fit = CovidModelFit(engine=engine, region_definitions=region_definitions, region=fit_params.region, tc_min=fit_params.tc_min, tc_max=fit_params.tc_max, **parser.specs_args_as_dict())
+    # regions can only be fit individually, so loop through regions if multiple are listed
+    regions = fit_params.regions
+    if fit_params.multiprocess:
+        args_list = map(lambda x: [x, parser.specs_args_as_dict(update={'regions': [x]}), parser.non_specs_args_as_dict(update={"batch_size": batch_size}), False], regions)
+        p = Pool(fit_params.multiprocess)
+        write_infos = p.map(do_fit_on_region, args_list)
 
-    # extract county IDs for the provided region; if statewide, set counties=None
-    if region is not None:
-        counties = json.load(open(region_definitions))[region]['county_names']
-        #counties = counties if type(counties) == list else [counties]
-        county_ids = json.load(open(region_definitions))[region]['county_fips']
-        #county_ids = county_ids if type(county_ids) == list else [county_ids]
+        # write results to database serially
+        for write_info in write_infos:
+            if write_info is not None:
+                CovidModelSpecifications.write_prepared_specs_to_db(write_info, db_engine())
     else:
-        counties, county_ids = (None, None)
-
-    # if hospitalization data CSV is provided, get hospitalizations from there; else query the database
-    if fit_params.hosp_data is not None:
-        if region is not None:
-            hosp_data = pd.read_csv(fit_params.hosp_data)[['date'] + counties]
-            tstart = pd.to_datetime(hosp_data['date']).min()
-            fit.base_specs.start_date = dt.date(tstart.year, tstart.month, tstart.day)
-            fit.actual_hosp = hosp_data.drop('date', axis=1).sum(axis=1)
-        else:
-            raise ValueError('Hospitalization data file is not supported for statewide model fitting.')
-    else:
-        fit.set_actual_hosp(engine, county_ids=county_ids)
-
-    # run fit
-    fit.run(engine, look_back=look_back, batch_size=batch_size,
-            increment_size=increment_size, window_size=window_size,
-            region_definitions=region_definitions, region=region,
-            write_batch_output=write_batch_output,
-            model_class=model_class,
-            model_args={"region": region} if region is not None else dict())
-
-    print(fit.fitted_model.tslices)
-    print(fit.fitted_tc)
-
-    fit.fitted_model.tags['run_type'] = 'fit'
-    fit.fitted_model.write_to_db(engine)
-
-    if fit_params.plot:
-        actual_hosps(engine, county_ids=county_ids)
-        modeled(fit.fitted_model, 'Ih')
-        plt.show()
+        args_list = map(lambda x: [x, parser.specs_args_as_dict(update={'regions': [x]}), parser.non_specs_args_as_dict(update={"batch_size": batch_size}), True], regions)
+        results = map(do_fit_on_region, args_list)
+        any(results) # force execution of the map
 
 
 if __name__ == '__main__':
