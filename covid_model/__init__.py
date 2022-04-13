79dd5b3f12bdec244150d0e1d229d67465f54f46
Alex Fout <fouticus@users.noreply.github.com>
2022-04-13 14:58:50 -0600

    deleted regional model

--- covid_model/__init__.py
+++ covid_model/__init__.py
@@ -1,5 +1,4 @@
 ### Python Standard Library ###
 ### Third Party Imports ###
 ### Local Imports ###
-from .model import CovidModel
-from .regional_model import RegionalCovidModel
+from covid_model.model import CovidModel
