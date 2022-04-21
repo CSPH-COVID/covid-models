f02648f02548fc3c5f4ae53c6a20ba17ec07da91
Alex Fout <fouticus@users.noreply.github.com>
2022-04-21 09:14:26 -0600

    updated plotting, removed params comparison, added saving of params to disk

--- covid_model/analysis/20220419_disconnected_vs_connected_test.py
+++ covid_model/analysis/20220419_disconnected_vs_connected_test.py
@@ -3,14 +3,18 @@ import os
 import datetime as dt
 import json
 ### Third Party Imports ###
-import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import matplotlib.dates as mdates
 ### Local Imports ###
 from covid_model.run_scripts import run_solve_seir, run_fit
 from covid_model.utils import get_file_prefix
 
 
 def main():
+    ####################################################################################################################
+    # Set Up Arguments for Running
     outdir = os.path.join("covid_model", "output", os.path.basename(__file__))
     os.makedirs(outdir, exist_ok=True)
 
@@ -21,7 +25,7 @@ def main():
         'tc_max': 0.99,
         'forward_sim_each_batch': True,
         'bsed': True,
-        'multiprocess': None,
+        'multiprocess': 2,
         'look_back': None,
         'use_base_specs_end_date': True,
         'window_size': 14,
@@ -37,7 +41,7 @@ def main():
         'refresh_actual_vacc': True,
         'refrech_actual_mobility': True,
         'regions': ['ms', 'cent'],
-        'start_date': dt.datetime.strptime('2020-02-24', "%Y-%m-%d").date(),
+        'start_date': dt.datetime.strptime('2020-03-01', "%Y-%m-%d").date(),
         'end_date': dt.datetime.strptime('2022-04-01', "%Y-%m-%d").date(),
         'tc': [0.75, 0.75],
         'tslices': [14],
@@ -47,14 +51,18 @@ def main():
         f.write(json.dumps(fit_args, default=str, indent=4))
         f.write(json.dumps(run_args, default=str, indent=4))
 
-    mob_modes = ['none', 'population_attached']
+
+    ####################################################################################################################
+    # Run
+    mob_modes = ['population_attached', 'none']
 
     print('Run fit')
-    #write_infos = run_fit(**fit_args, **run_args, outdir=outdir)
     ms = run_fit(**fit_args, **run_args, outdir=outdir)
-    run_args['region_fit_spec_ids'] = [x['spec_id'] if isinstance(x, dict) else x.spec_id for x in ms]
-    #run_args['region_fit_spec_ids'] = [2080, 2081]
+    run_args['region_fit_spec_ids'] = [x['specs_info']['spec_id'] if isinstance(x, dict) else x.spec_id for x in ms]
+    run_args['region_fit_result_ids'] = [x['results_info']['result_id'].iloc[0].item() if isinstance(x, dict) else x.result_id for x in ms]
 
+    #run_args['region_fit_spec_ids'] = [2214, 2215]
+    #run_args['region_fit_result_ids'] = [144, 145]
 
     dfs = []
     dfhs = []
@@ -62,46 +70,32 @@ def main():
     for mm in mob_modes:
         print(f'Mobility Mode: {mm}')
         print('Run Forward Sim')
-        run_args.update({'mobility_mode': mm, 'refresh_act ual_mobility': None if mm == 'none' else True})
+        run_args.update({'mobility_mode': mm, 'refresh_actual_mobility': None if mm == 'none' else True})
         model, df, dfh = run_solve_seir(**run_args, outdir=outdir)
         ms2.append(model)
         dfs.append(df.assign(mobility=mm))
         dfhs.append(dfh.assign(mobility=mm))
+        with open(get_file_prefix(outdir) + f'{mm}_ode_terms.json', 'w') as f:
+            f.write(model.ode_terms_as_json())
     df = pd.concat(dfs).reset_index(drop=True)
     dfh = pd.concat(dfhs, axis=0)
 
 
     print("saving results")
-    df.to_csv(get_file_prefix(outdir) + "fit_vs_run_run_compartments.csv")
-    dfh.to_csv(get_file_prefix(outdir) + "fit_vs_run_run_forwardsim_hospitalized.csv")
+    df.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_run_compartments.csv")
+    dfh.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_run_forwardsim_hospitalized.csv")
 
-    #print("plotting results")
-    #p = sns.relplot(data=df, x='date', y='y', hue='mobility', col='region', row='seir', kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
-    #_ = [ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())) for ax in p.axes.flat]
-    #plt.savefig(get_file_prefix(outdir) + "fit_vs_run_test_compartments.png", dpi=300)
+    print("plotting results")
+    p = sns.relplot(data=df, x='date', y='y', hue='mobility', col='region', row='seir', kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
+    _ = [ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())) for ax in p.axes.flat]
+    plt.savefig(get_file_prefix(outdir) + "disconnected_vs_connected_test_compartments.png", dpi=300)
 
-    #p = sns.relplot(data=dfh, x='date', y='hospitalized', hue='mode', col='region', col_wrap=min(3, len(run_args['regions'])), kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
-    #_ = [ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())) for ax in p.axes.flat]
-    #plt.savefig(get_file_prefix(outdir) + "fit_vs_run_test_hospitalized.png", dpi=300)
-
-    # compare DEQ components from the disconnected models to the connected model
-    idxs = [[ms2[0].cmpt_idx_lookup[cmpt] for cmpt in m.compartments] for m in ms]
-    comparisons = [None] * len(ms)
-    for i, tup in enumerate(zip(ms, idxs)):
-        comparisons[i] = {}
-        m, idx = tup
-        for t in m.trange:
-            comparisons[i][t] = {}
-            comparisons[i][t]['const_vector'] = ([ms2[0].constant_vector[t][idx]] == m.constant_vector[t]).all()
-            comparisons[i][t]['linear_matrix'] = (ms2[0].linear_matrix[t].toarray()[idx, :][:, idx] == m.linear_matrix[t].toarray()).all()
-            comparisons[i][t]['nonlinear_matrices'] = []
-            for k in m.nonlinear_matrices[t].keys():
-                k2 = tuple(idx[x] for x in k)
-                comparisons[i][t]['nonlinear_matrices'].append(np.allclose(ms2[0].nonlinear_matrices[t][k2].toarray()[idx, :][:, idx] * ms2[0].nonlinear_multiplier[t], m.nonlinear_matrices[t][k].toarray() * m.nonlinear_multiplier[t]))
-
-    print([v['const_vector'] for c in comparisons for v in c.values()])
-    print([v['linear_matrix'] for c in comparisons for v in c.values()])
-    print([tv for c in comparisons for v in c.values() for tv in v['nonlinear_matrices']])
+    dfh_measured = dfh[['currently_hospitalized']].rename(columns={'currently_hospitalized': 'hospitalized'}).loc[dfh['mobility'] == 'none'].assign(series='observed')
+    dfh_modeled = dfh[['modeled_hospitalized', 'mobility']].rename(columns={'modeled_hospitalized': 'hospitalized', 'mobility': 'series'})
+    dfh2 = pd.concat([dfh_measured,dfh_modeled], axis=0).set_index('series', append=True)
+    p = sns.relplot(data=dfh2, x='date', y='hospitalized', hue='series', col='region', col_wrap=min(3, len(run_args['regions'])), kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
+    _ = [ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())) for ax in p.axes.flat]
+    plt.savefig(get_file_prefix(outdir) + "disconnected_vs_connected_test_hospitalized.png", dpi=300)
 
     print("done")
 
