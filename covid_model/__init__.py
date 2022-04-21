f4b9977cb07c04a9a8b8e4e2d24fe065f86a1009
Alex Fout <fouticus@users.noreply.github.com>
2022-04-21 09:09:20 -0600

    imported much more from covid_model into module

--- covid_model/__init__.py
+++ covid_model/__init__.py
@@ -1,4 +1,14 @@
 ### Python Standard Library ###
+# bad idea to import here
 ### Third Party Imports ###
+# bad idea to import here
 ### Local Imports ###
+# classes
 from covid_model.model import CovidModel
+from covid_model.model_specs import CovidModelSpecifications
+from covid_model.model_fit import CovidModelFit
+from covid_model.cli_specs import ModelSpecsArgumentParser
+from covid_model.ode_builder import ODEBuilder
+from covid_model.model_sims import CovidModelSimulation
+# functions
+from covid_model.db import db_engine
