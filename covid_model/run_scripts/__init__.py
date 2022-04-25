f3aae3367b3992eab8145b93976102118c322ace
Alex Fout <fouticus@users.noreply.github.com>
2022-04-25 15:39:20 -0600

    added compartment report to run scripts

--- covid_model/run_scripts/__init__.py
+++ covid_model/run_scripts/__init__.py
@@ -1,3 +1,4 @@
 from covid_model.run_scripts.run_fit import run_fit
 from covid_model.run_scripts.run_solve_seir import run_solve_seir
 from covid_model.run_scripts.run_model_scenarios import run_model_scenarios
+from covid_model.run_scripts.run_compartment_report import run_compartment_report
