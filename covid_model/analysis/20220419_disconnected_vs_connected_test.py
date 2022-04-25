c00f8978b65fc34e454ebcbe47920290facc09ce
Alex Fout <fouticus@users.noreply.github.com>
2022-04-24 19:50:39 -0600

    added plotting of kappas, backed up start date to accommodate seeds for some regions. renamed run_args to specs_args.

--- covid_model/analysis/20220419_disconnected_vs_connected_test.py
+++ covid_model/analysis/20220419_disconnected_vs_connected_test.py
@@ -24,14 +24,13 @@ def main():
         'tc_min': -0.99,
         'tc_max': 0.99,
         'forward_sim_each_batch': True,
-        'bsed': True,
-        'multiprocess': 2,
+        'multiprocess': 4,
         'look_back': None,
         'use_base_specs_end_date': True,
         'window_size': 14,
         'write_batch_output': False
     }
-    run_args = {
+    specs_args = {
         'params': 'covid_model/input/params.json',
         'region_definitions': 'covid_model/input/region_definitions.json',
         'timeseries_effect_multipliers': 'covid_model/input/timeseries_effects/multipliers.json',
@@ -40,29 +39,30 @@ def main():
         'vacc_proj_params': 'covid_model/input/vacc_proj_params.json',
         'refresh_actual_vacc': True,
         'refrech_actual_mobility': True,
-        'regions': ['ms', 'cent'],
-        'start_date': dt.datetime.strptime('2020-03-01', "%Y-%m-%d").date(),
+        'regions': ['met', 'ms', 'ne'],
+        'start_date': dt.datetime.strptime('2020-02-19', "%Y-%m-%d").date(),
         'end_date': dt.datetime.strptime('2022-04-01', "%Y-%m-%d").date(),
         'tc': [0.75, 0.75],
         'tslices': [14],
         'max_step_size': 0.5
     }
     with open(get_file_prefix(outdir) + "________________________.txt", 'w') as f:
-        f.write(json.dumps(fit_args, default=str, indent=4))
-        f.write(json.dumps(run_args, default=str, indent=4))
+        f.write(json.dumps({"fit_args": fit_args, "spec_args": specs_args}, default=str, indent=4))
 
 
     ####################################################################################################################
     # Run
+    # don't change the order of these unless you also change the kappa comparison
     mob_modes = ['population_attached', 'none']
+    which_kappa = ['kappa_pa', 'kappa']
 
     print('Run fit')
-    #ms = run_fit(**fit_args, **run_args, outdir=outdir)
-    #run_args['region_fit_spec_ids'] = [x['specs_info']['spec_id'] if isinstance(x, dict) else x.spec_id for x in ms]
-    #run_args['region_fit_result_ids'] = [x['results_info']['result_id'].iloc[0].item() if isinstance(x, dict) else x.result_id for x in ms]
+    ms = run_fit(**fit_args, **specs_args, outdir=outdir)
+    specs_args['region_fit_spec_ids'] = [x['specs_info']['spec_id'] if isinstance(x, dict) else x.spec_id for x in ms]
+    specs_args['region_fit_result_ids'] = [x['results_info']['result_id'].iloc[0].item() if isinstance(x, dict) else x.result_id for x in ms]
 
-    run_args['region_fit_spec_ids'] = [2265, 2266]
-    run_args['region_fit_result_ids'] = [192, 193]
+    #specs_args['region_fit_spec_ids'] = [2278, 2279]
+    #specs_args['region_fit_result_ids'] = [205, 206]
 
     dfs = []
     dfhs = []
@@ -70,8 +70,8 @@ def main():
     for mm in mob_modes:
         print(f'Mobility Mode: {mm}')
         print('Run Forward Sim')
-        run_args.update({'mobility_mode': mm, 'refresh_actual_mobility': None if mm == 'none' else True})
-        model, df, dfh = run_solve_seir(**run_args, outdir=outdir)
+        specs_args.update({'mobility_mode': mm, 'refresh_actual_mobility': None if mm == 'none' else True})
+        model, df, dfh = run_solve_seir(**specs_args, outdir=outdir)
         ms2.append(model)
         dfs.append(df.assign(mobility=mm))
         dfhs.append(dfh.assign(mobility=mm))
@@ -79,12 +79,19 @@ def main():
             f.write(model.ode_terms_as_json())
     df = pd.concat(dfs).reset_index(drop=True)
     dfh = pd.concat(dfhs, axis=0)
+    dfk = pd.concat([
+        m.params_as_df[[kn]].groupby(['t', 'region']).mean().
+            rename(columns={'kappa_pa': 'kappa'}).assign(mobility=mm) for m, kn, mm in zip(ms2, which_kappa, mob_modes)
+    ], axis=0)
+    dfk.index = dfk.index.set_levels(ms2[0].daterange, level=0)
+    dfk = dfk.set_index('mobility', append=True)
 
     # TODO: compare kappa across mobility modes
 
     print("saving results")
-    df.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_run_compartments.csv")
-    dfh.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_run_forwardsim_hospitalized.csv")
+    df.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_test_compartments.csv")
+    dfh.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_test_hospitalized.csv")
+    dfk.to_csv(get_file_prefix(outdir) + "disconnected_vs_connected_test_kappa.csv")
 
     print("plotting results")
     p = sns.relplot(data=df, x='date', y='y', hue='mobility', col='region', row='seir', kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
@@ -94,9 +101,13 @@ def main():
     dfh_measured = dfh[['currently_hospitalized']].rename(columns={'currently_hospitalized': 'hospitalized'}).loc[dfh['mobility'] == 'none'].assign(series='observed')
     dfh_modeled = dfh[['modeled_hospitalized', 'mobility']].rename(columns={'modeled_hospitalized': 'hospitalized', 'mobility': 'series'})
     dfh2 = pd.concat([dfh_measured,dfh_modeled], axis=0).set_index('series', append=True)
-    p = sns.relplot(data=dfh2, x='date', y='hospitalized', hue='series', col='region', col_wrap=min(3, len(run_args['regions'])), kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
+    p = sns.relplot(data=dfh2, x='date', y='hospitalized', hue='series', col='region', col_wrap=min(3, len(specs_args['regions'])), kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
     _ = [ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())) for ax in p.axes.flat]
     plt.savefig(get_file_prefix(outdir) + "disconnected_vs_connected_test_hospitalized.png", dpi=300)
+
+    p = sns.relplot(data=dfk, x='t', y='kappa', hue='mobility', style='mobility', col='region', col_wrap=min(3, len(specs_args['regions'])), kind='line', facet_kws={'sharex': False, 'sharey': False}, height=2, aspect=4)
+    _ = [ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator())) for ax in p.axes.flat]
+    plt.savefig(get_file_prefix(outdir) + "disconnected_vs_connected_test_kappa.png", dpi=300)
 
     print("done")
 
