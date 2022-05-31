4170c14b203bddbfa025e995820c3a713dda1111
Alex Fout <fouticus@users.noreply.github.com>
2022-05-31 09:37:10 -0600

    new script for running projections with lack of priorinf compartment.

---
+++ covid_model/analysis/20220525_model_simplification/20220531_projections.py
@@ -0,0 +1,59 @@
+### Python Standard Library ###
+import copy
+import os
+import datetime as dt
+import json
+import logging
+import numpy as np
+### Third Party Imports ###
+from collections import OrderedDict
+from matplotlib import pyplot as plt
+### Local Imports ###
+from covid_model import CovidModel
+#from covid_model.runnable_functions import
+from covid_model.utils import IndentLogger, setup, get_filepath_prefix
+logger = IndentLogger(logging.getLogger(''), {})
+from covid_model.db import db_engine
+from covid_model.analysis.charts import plot_transmission_control
+
+def main():
+    ####################################################################################################################
+    # Set Up Arguments for Running
+    outdir = setup(os.path.basename(__file__), 'info')
+
+    model_args = {'base_spec_id': 2645, 'end_date': "2022-09-01"}
+    logging.info(json.dumps({"model_args": model_args}, default=str))
+
+    ####################################################################################################################
+    # Run
+
+    # fit a statewide model up to present day to act as a baseline
+    logging.info('Fitting')
+
+    model = CovidModel(**model_args)
+    model.prep()
+    model.solve_seir()
+    model.immunity()
+
+    logger.info(f'{str(model.tags)}: Running forward sim')
+    fig = plt.figure(figsize=(10, 10), dpi=300)
+    ax = fig.add_subplot(211)
+    hosps_df = model.modeled_vs_actual_hosps().reset_index('region').drop(columns='region')
+    hosps_df.plot(ax=ax)
+    ax = fig.add_subplot(212)
+    plot_transmission_control(model, ax=ax)
+    plt.savefig(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_fit.png')
+    plt.close()
+    hosps_df.to_csv(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_fit.csv')
+    json.dump(dict(dict(zip([0] + model.tc_tslices, model.tc))), open(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_tc.json', 'w'))
+
+    logger.info(f'{str(model.tags)}: Uploading final results')
+    engine = db_engine()
+    model.write_specs_to_db(engine)
+    model.write_results_to_db(engine)
+    logger.info(f'{str(model.tags)}: spec_id: {model.spec_id}')
+    model.solution_sum(['seir', 'variant', 'immun']).unstack().to_csv(get_filepath_prefix(outdir) + "states_seir_variant_immun.csv")
+
+
+if __name__ == "__main__":
+    main()
