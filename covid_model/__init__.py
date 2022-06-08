179b576040a974cb734550724e5f191ff01fb4de
Alex Fout <fouticus@users.noreply.github.com>
2022-06-08 13:56:41 -0600

    removed this class

--- covid_model/__init__.py
+++ covid_model/__init__.py
@@ -5,7 +5,6 @@
 ### Local Imports ###
 # classes
 from covid_model.model import CovidModel
-from covid_model.cli_specs import ModelSpecsArgumentParser
 from covid_model.model_sims import CovidModelSimulation
 # functions
 from covid_model.db import db_engine
