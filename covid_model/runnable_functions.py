7ed18c8d48f8c31c3133f67a64c517aae4bde6f9
Alex Fout <fouticus@users.noreply.github.com>
2022-06-12 17:11:42 -0600

    reworked fitting so a new model doesn't have to be created for each batch. instead, start and end dates are specified. Also look_back doesn't exist anymore, in favor of dates.

--- covid_model/runnable_functions.py
+++ covid_model/runnable_functions.py
@@ -21,47 +21,45 @@ from covid_model.analysis.charts import plot_modeled, plot_observed_hosps, forma
 logger = IndentLogger(logging.getLogger(''), {})
 
 
-def __single_window_fit(model: CovidModel, look_back, tc_min, tc_max, yd_start=None, tstart=None):
+def __single_batch_fit(model: CovidModel, tc_min, tc_max, yd_start=None, tstart=None, tend=None):
     # define initial states
-    fixed_tc = model.tc[:-look_back]
+    tc = {t: model.tc[t] for t in model.tc.keys() if t >= tstart and t <= tend}
+    tc_ts  = list(tc.keys())
     yd_start = model.y0_dict if yd_start is None else yd_start
     y0 = model.y0_from_dict(yd_start)
-    trange = range(tstart, model.tmax+1)
+    trange = range(tstart, tend+1)
 
     # function to be optimized
     def func(trange, *test_tc):
-        combined_tc = fixed_tc + list(test_tc)
-        model.apply_tc(combined_tc)
-        model.solve_seir(y0=y0, t0=tstart, method='RK45')
-        return model.solution_sum_Ih(tstart)
+        for i, tc_val in enumerate(test_tc):
+            tc[tc_ts[i]] = tc_val
+        model.update_tc(tc, replace=False, update_lookup=False)
+        model.solve_seir(y0=y0, tstart=tstart, tend=tend, method='RK45')
+        return model.solution_sum_Ih(tstart, tend)
     fitted_tc, fitted_tc_cov = spo.curve_fit(
-        f=func
-        , xdata=trange
-        , ydata=model.estimated_actual_hosp[tstart:(tstart + len(trange))]
-        , p0=model.tc[-look_back:]
-        , bounds=([tc_min] * look_back, [tc_max] * look_back))
+        f=func,
+        xdata=trange,
+        ydata=model.estimated_actual_hosp[trange],
+        p0=[tc[t] for t in tc_ts],
+        bounds=([tc_min] * len(tc_ts), [tc_max] * len(tc_ts)))
     return fitted_tc, fitted_tc_cov
 
-
+# fits TC for the model between two dates, and does the fit in batches to make the solving easier
 def do_single_fit(tc_0=0.75,  # default value for TC
                   tc_min=0,  # minimum allowable TC
                   tc_max=0.99,  # maximum allowable TC
-                  window_size=14,  # How often to update TC (days)
-                  look_back=None,  # How many tc values to refit (if None, refit all) (if refit_from_date is not None this must be None)
-                  refit_from_date=None, # refit all tc's on or after this date (if look_back is not None this must be None)
-                  last_window_min_size=21,  # smallest size of the last TC window
-                  batch_size=None,  # How many windows to fit at once
-                  increment_size=1,  # How many windows to shift over for each fit
+                  tc_window_size=14,  # How often to update TC (days)
+                  tc_window_batch_size=5,  # How many windows to fit at once
+                  tc_batch_increment=1,  # How many TC windows to shift over for each batch fit
+                  last_tc_window_min_size=21,  # smallest size of the last TC window
+                  fit_start_date=None,  # refit all tc's on or after this date (if None, use model start date)
+                  fit_end_date=None,  # refit all tc's up to this date (if None, uses either model end date or last date with hospitalization data, whichever is earlier
+                  loss_projection_days=None,  # can compute loss of fit using only the batch window (if =0), or also include a projection (in days) into the future. If None, will project until fit_end_date
                   prep_model=True,  # Should we run model.prep
-                  use_hosps_end_date=True,  # Should we fit all the way to the end_date of the hospitalization data (as opposed to the model's end date)
                   outdir=None,  # the output directory for saving results
                   write_results=True,  # should final results be written to the database
                   write_batch_results=False,  # Should we write output to the database after each fit
                   **model_args):
-    # data checks
-    if look_back is not None and refit_from_date is not None:
-        logger.exception(f"do_single_fit: Cannot specify both look_back and refit_from_date")
-        raise ValueError(f"do_single_fit: Cannot specify both look_back and refit_from_date")
 
     def forward_sim_plot(model):
         # TODO: refactor into charts?
@@ -75,70 +73,58 @@ def do_single_fit(tc_0=0.75,  # default value for TC
         plt.savefig(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_fit.png')
         plt.close()
         hosps_df.to_csv(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_fit.csv')
-        json.dump(dict(dict(zip([0] + model.tc_tslices, model.tc))), open(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_tc.json', 'w'))
+        json.dump(dict(model.tc), open(get_filepath_prefix(outdir) + f'{"_".join(str(key) + "_" + str(val) for key, val in model.tags.items())}_model_tc.json', 'w'))
 
-#    logging.info(json.dumps({"fit_args": fit_args}, default=str))
-    logging.info(str({"model_build_args": model_args}))
+    logging.debug(str({"model_build_args": model_args}))
 
-    # initialize and run the fit
+    # get a db connection if we're going to be writing results
     if write_batch_results or write_results:
         engine = db_engine()
 
-    base_model = CovidModel(**model_args)
+    model = CovidModel(**model_args)
 
-    # get the end date from actual hosps
-    if use_hosps_end_date:
-        base_model.end_date = base_model.observed_hosp.index.max()
-    elif base_model.observed_hosp.index.max() < base_model.end_date:
-        ermsg = f'Opted to fit to model end_date, {base_model.end_date}, but hospitalizations only available up to {base_model.observed_hosp.index.max()}'
-        logger.exception(f"{str(base_model.tags)}" + ermsg)
+    # adjust fit start and end, and check for consistency with model and hosp dates
+    fit_start_date = model.start_date if fit_start_date is None else fit_start_date
+    fit_end_date = min(model.end_date, model.observed_hosp.index.max()) if fit_end_date is None else fit_end_date
+    ermsg = None
+    if fit_start_date < model.start_date:
+        ermsg = f'Fit needs to start on or after model start date. Opted to start fitting at {fit_start_date} but model start date is {model.start_date}'
+    elif fit_end_date > model.end_date:
+        ermsg = f'Fit needs to end on or before model end date. Opted to stop fitting at {fit_end_date} but model end date is {model.end_date}'
+    elif fit_end_date > model.observed_hosp.index.max():
+        ermsg = f'Fit needs to end on or before last date with hospitalization data. Opted to stop fitting at {fit_end_date} but last date with hospitalization data is {model.observed_hosp.index.max()}'
+    if ermsg is not None:
+        logger.exception(f"{str(model.tags)}" + ermsg)
         raise ValueError(ermsg)
 
     # prep model (we only do this once to save time)
     if prep_model:
-        logger.info(f'{str(base_model.tags)} Prepping Model')
+        logger.info(f'{str(model.tags)} Prepping Model')
         t0 = perf_counter()
-        base_model.prep()
-        logger.debug(f'{str(base_model.tags)} Model flows {base_model.flows_string}')
-        logger.info(f'{str(base_model.tags)} Model prepped for fitting in {perf_counter() - t0} seconds.')
+        model.prep()
+        logger.debug(f'{str(model.tags)} Model flows {model.flows_string}')
+        logger.info(f'{str(model.tags)} Model prepped for fitting in {perf_counter() - t0} seconds.')
 
-    # Apply TC
-    tslices = base_model.tc_tslices  + list(range((base_model.tc_tslices[-1] if len(base_model.tc_tslices) > 0 else 0) + window_size, base_model.tmax - last_window_min_size, window_size))
-    tc = base_model.tc + [tc_0] * (len(tslices) + 1 - len(base_model.tc))
-    base_model.apply_tc(tcs=tc, tslices=tslices)
+    # replace the TC and tslices within the fit window
+    fit_tstart = model.date_to_t(fit_start_date)
+    fit_tend = model.date_to_t(fit_end_date)
+    tc = {t: tc for t, tc in model.tc.items() if t < fit_tstart or t > fit_tend}
+    tc.update({t: tc_0 for t in range(fit_tstart, fit_tend - last_tc_window_min_size, tc_window_size)})
+    model.update_tc(tc)
 
-    # run fit
-    fitted_tc_cov = None
-    if look_back is None and refit_from_date is None:
-        look_back = len(tslices) + 1
-    elif refit_from_date is not None:
-        refit_from_date = refit_from_date if isinstance(refit_from_date, dt.date) else dt.datetime.strptime(refit_from_date, "%Y-%m-%d").date()
-        t_cutoff = base_model.date_to_t(refit_from_date)
-        look_back = len([t for t in [0] + tslices if t >= t_cutoff])
+    # Get start/end for each batch
+    relevant_tc_ts = [t for t in model.tc.keys() if t >= fit_tstart and t <= fit_tend]
+    batch_tstarts =  relevant_tc_ts[:-tc_window_batch_size:tc_batch_increment] + [relevant_tc_ts[-tc_window_batch_size]]
+    batch_tends = [t - 1 for t in relevant_tc_ts[tc_window_batch_size::tc_batch_increment]] + [fit_tend]
 
-    # if there's no batch size, set the batch size to be the total number of windows to be fit
-    if batch_size is None or batch_size > look_back:
-        batch_size = look_back
-
-    trim_off_end_list = list(range(look_back - batch_size, 0, -increment_size)) + [0]
-    logger.info(f'{str(base_model.tags)} Will fit {len(trim_off_end_list)} times')
-    for i, trim_off_end in enumerate(trim_off_end_list):
+    logger.info(f'{str(model.tags)} Will fit {len(batch_tstarts)} times')
+    for i, (tstart, tend) in enumerate(zip(batch_tstarts, batch_tends)):
+        tend = min(fit_tend, tend + loss_projection_days) if loss_projection_days is not None else fit_tend
         t0 = perf_counter()
-        this_end_t = tslices[-trim_off_end] if trim_off_end > 0 else base_model.tmax
-        this_end_date = base_model.t_to_date(this_end_t)
-
-        t01 = perf_counter()
-        model = CovidModel(base_model=base_model, end_date=this_end_date, update_derived_properties=False)
-        model.apply_tc(tc[:len(tc) - trim_off_end], tslices=tslices[:len(tslices) - trim_off_end])
-        logger.info(f'{str(model.tags)}: Model copied in {perf_counter() - t01} seconds.')
-
-        tstart = ([0] + model.tc_tslices)[-batch_size]
         yd_start = model.y_dict(tstart) if tstart != 0 else model.y0_dict
-        fitted_tc, fitted_tc_cov = __single_window_fit(model, look_back=batch_size, tc_min=tc_min, tc_max=tc_max, yd_start=yd_start, tstart=tstart)
-        base_model.solution_y = model.solution_y
-        tc[len(tc) - trim_off_end - batch_size:len(tc) - trim_off_end] = fitted_tc
-        logger.info(f'{str(model.tags)}: Transmission control fit {i + 1}/{len(trim_off_end_list)} completed in {perf_counter() - t0} seconds: {fitted_tc}')
+        fitted_tc, fitted_tc_cov = __single_batch_fit(model, tc_min=tc_min, tc_max=tc_max, yd_start=yd_start, tstart=tstart, tend=tend)
         model.tags['fit_batch'] = str(i)
+        logger.info(f'{str(model.tags)}: Transmission control fit {i + 1}/{len(batch_tstarts)} completed in {perf_counter() - t0} seconds: {fitted_tc}')
 
         if write_batch_results:
             logger.info(f'{str(model.tags)}: Uploading batch results')
@@ -150,14 +136,13 @@ def do_single_fit(tc_0=0.75,  # default value for TC
 
     model.tc_cov = fitted_tc_cov
     model.tags['run_type'] = 'fit'
-    logger.info(f'{str(model.tags)}: tslices: {str(model.tc_tslices)}')
-    logger.info(f'{str(model.tags)}: fitted TC: {str(fitted_tc)}')
+    logger.info(f'{str(model.tags)}: fitted TC: {model.tc}')
 
     if outdir is not None:
         forward_sim_plot(model)
 
     if write_results:
-        logger.info(f'{str(base_model.tags)}: Uploading final results')
+        logger.info(f'{str(model.tags)}: Uploading final results')
         model.write_specs_to_db(engine)
         model.write_results_to_db(engine)
         logger.info(f'{str(model.tags)}: spec_id: {model.spec_id}')
@@ -269,17 +254,17 @@ def do_create_report(model, outdir, from_date=None, to_date=None, prep_model=Fal
 
 
 def do_build_legacy_output_df(model: CovidModel):
-    ydf = model.solution_sum(['seir', 'age', 'region']).stack(level='age')
+    ydf = model.solution_sum_df(['seir', 'age', 'region']).stack(level='age')
     df = ydf.unstack(level=1).stack(level=1)
 
     alpha_df = model.get_param_for_attrs_by_t('alpha', attrs={})
     alpha_df['date'] = model.daterange
     alpha_df = alpha_df.reset_index('t').set_index('date', append=True).drop(columns='t')
-    combined = model.solution_sum()[['E']].stack(model.param_attr_names).join(alpha_df)
+    combined = model.solution_sum_df()[['E']].stack(model.param_attr_names).join(alpha_df)
     combined['Einc'] = (combined['E'] / combined['alpha'])
     combined = combined.groupby(['date', 'region']).sum()
 
-    totals = model.solution_sum(['seir', 'region']).stack(level=1)
+    totals = model.solution_sum_df(['seir', 'region']).stack(level=1)
     totals = totals.rename(columns={'Ih': 'Iht', 'D': 'Dt', 'E': 'Etotal'})
     totals['Itotal'] = totals['I'] + totals['A']
 
