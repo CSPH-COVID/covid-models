08a823d5aa4a31b357be2f5886a7aabe9b973e46
emwu9912 <emwu9912@colorado.edu>
2022-07-06 17:14:11 -0600

    updating vaccination input data to include second booster

--- covid_model/sql/vaccination_by_age_group_with_boosters_wide.sql
+++ covid_model/sql/vaccination_by_age_group_with_boosters_wide.sql
@@ -3,5 +3,6 @@ select
     , age_group as age
 	, dose1_mrna + dose1_jnj as shot1
 	, dose2_mrna as shot2
-	, booster as shot3
+	, booster1 as booster1
+	, booster2 as booster2
 from vaccination.combined_doses_by_age_group v;
