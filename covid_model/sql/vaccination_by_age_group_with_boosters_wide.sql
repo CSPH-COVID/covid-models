6d7ceaddf4dcf312aa3fc3205e564f7c3c97c245
emwu9912 <emwu9912@colorado.edu>
2022-10-03 17:07:44 -0600

    removing booster3 variable from query

--- covid_model/sql/vaccination_by_age_group_with_boosters_wide.sql
+++ covid_model/sql/vaccination_by_age_group_with_boosters_wide.sql
@@ -5,5 +5,4 @@ select
 	, dose2_mrna as shot2
 	, booster1 as booster1
 	, booster2 as booster2
-	, booster3 as booster3
-from vaccination.combined_doses_by_age_group_withbooster3 v;
+from vaccination.combined_doses_by_age_group v;
